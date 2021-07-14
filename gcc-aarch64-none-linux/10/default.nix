44c2b7ff72d379a60af8fad81a50afdba7b1845a
polygon <polygon@wh2.tu-dresden.de>
2021-07-15 00:20:06 +0200

    Speed up patchelf process

--- gcc-aarch64-none-linux/10/default.nix
+++ gcc-aarch64-none-linux/10/default.nix
@@ -32,7 +32,7 @@ stdenv.mkDerivation rec {
   '';
 
   preFixup = ''
-    find $out -type f | while read f; do
+    find $out -executable -type f | while read f; do
       patchelf "$f" > /dev/null 2>&1 || continue
       patchelf --set-interpreter $(cat ${stdenv.cc}/nix-support/dynamic-linker) "$f" || true
       patchelf --set-rpath ${lib.makeLibraryPath [ "$out" stdenv.cc.cc ncurses5 python27 expat ]} "$f" || true
